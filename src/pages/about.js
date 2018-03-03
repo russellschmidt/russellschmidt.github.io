a74fa268180bb28d072f0d9a4816915f9385845a
Russell Schmidt <russell.schmidt@me.com>
2018-03-03 00:09:26 -0800

    add form styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,12 +4,43 @@ import Helmet from "react-helmet";
 import g from "glamorous";
 import { css } from "glamor";
 
+import { rhythm } from "../utils/typography";
+
+let container = css({
+  height: `calc(100vh - 248px)`,
+});
+
+const blogPostsContainer = css({
+  display: `grid`,
+  gridTemplateColumns: `repeat(12, 8%)`,
+  gridTemplateRows: `auto`,
+  width: `100%`,
+  justifyContent: `space-evenly`,
+});
+
+let imageContainer = css({
+
+});
+
+let sidebarContainer = css({
+
+});
+
+let articleContainer = css({
+
+});
+
 export default () => (
-  <div>
+  <div className={container}>
     <Helmet title={`About Russell Schmidt, a developer in Los Angeles, CA`}/>
     <h1>About</h1>
-    <p>
-      <a href="mailto:me@example.com">me@example.com</a>
-    </p>
+    <div className={blogPostsContainer}>
+      <div className={imageContainer}>
+      </div>
+      <div className={sidebarContainer}>
+      </div>
+      <div className={articleContainer}>
+      </div>
+    </div>
   </div>
 );
